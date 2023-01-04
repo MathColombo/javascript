aa2247e90646afe037906a8f6ae293780eaa3220
Matheus Colombo <matheus.colombo21@gmail.com>
2023-01-04 14:20:43 -0300

    .

--- exercicios/módulo D/horadodia/script.js
+++ exercicios/módulo D/horadodia/script.js
@@ -1,21 +1,23 @@
-function carregar() {
+function load() {
 
-    var msg = window.document.querySelectorAll('div#msg')
-    var img = window.document.querySelectorAll('div#imagem')
-    //var data = new Date()
-    //var hora = data.getHours()
-    var hora = 22
+    var msg = window.document.querySelector('div#msg')
+    var img = window.document.querySelector('img#imagem')
+    var data = new Date()
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
 
     if (hora < 12) {
         //Bom dia
         img.src = 'manha.png'
+        document.body.style.background = '#D7BB97'
     } else if (hora < 18) {
         //Boa tarde
         img.src = 'tarde.png'
+        document.body.style.background = '#CA3400'
     } else {
         //Boa noite
         img.src = 'noite.png'
+        document.body.style.background = '#022147'
     }
 
 }
