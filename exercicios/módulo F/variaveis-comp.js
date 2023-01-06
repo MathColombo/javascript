7f85b7a2373c88163138b75d6500e64a76cccafc
Matheus Colombo <matheus.colombo21@gmail.com>
2023-01-06 15:57:01 -0300

    ..

---
+++ exercicios/módulo F/variaveis-comp.js
@@ -0,0 +1,14 @@
+var num = [8, 1, 9, 1, 3]
+
+/*
+for(let pos = 0 ; pos < num.length; pos++) {
+
+    console.log(`${num[pos]}`)
+}
+
+for(let pos in num) {
+    console.log(`${num[pos]}`)
+}
+*/
+
+console.log(num.indexOf(6))
